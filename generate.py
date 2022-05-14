5d40ce8d506dea86c2ab89513147390fb8d6ba5d
Meteor <coopticks@outlook.com>
2022-05-14 22:12:16 +0800

    Add files via upload

--- generate.py
+++ generate.py
@@ -1,16 +1,26 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+
 import requests
+import shutil
+import os
 
 r = requests.get("https://ruleset.skk.moe/List/non_ip/stream.conf", stream=True)
 d = requests.get("https://ruleset.skk.moe/List/non_ip/domestic.conf", stream=True)
-with open("stream.conf", "a") as f:
+rule_file = os.path.dirname(os.path.realpath(__file__))+"/rules.txt"
+if os.path.isfile(rule_file):
+    os.remove(rule_file)
+shutil.copyfile("/home/wwwroot/network.tw/rules-model.txt", rule_file)
+with open(rule_file, "r") as f:
     # Domestic
+    rules = f.read()
+with open(rule_file, "w") as f:
+    toappend = ""
     for dline in d.iter_lines():
-        if "DOMAIN" in dline.decode('utf-8'):
-            f.write("  - "+dline.decode('utf-8')+",China\n")
+        if "DOMAIN" in dline.decode('utf-8') and "bili" not in dline.decode('utf-8') and "acg" not in dline.decode('utf-8') and "hdslb" not in dline.decode('utf-8') and "#" not in dline.decode('utf-8'):
+            toappend += "  - "+dline.decode('utf-8')+",China\n"
     # Stream Media
     for line in r.iter_lines():
         if "DOMAIN" in line.decode('utf-8') and "youtube" not in line.decode('utf-8') and "google" not in line.decode('utf-8'):
-            f.write("  - "+line.decode('utf-8')+",Media\n")
-
+            toappend += "  - "+line.decode('utf-8')+",海外流媒体\n"
+    f.write(rules.format(rule=toappend))
